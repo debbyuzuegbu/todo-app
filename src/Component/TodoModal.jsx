7c4937569cb8901cce51db22b73e502824805dde
Deborah <debbyuzuegbu@gmail.com>
2023-01-03 20:50:10 +0100

    added buttons

--- src/Component/TodoModal.jsx
+++ src/Component/TodoModal.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { MdOutlineClose } from 'react-icons/md';
 import styles from '../styles/modules/modal.module.scss';
+import Button from './Button';
 
 function TodoModal() {
   return (
@@ -9,8 +10,27 @@ function TodoModal() {
         <div className={styles.closeButton}>
           <MdOutlineClose />
         </div>
-        <form>
+        <form className={styles.form}>
           <h1 className={styles.formTitle}>Add Task</h1>
+          <label htmlFor="title">
+            Title
+            <input type="text" id="title" />
+          </label>
+          <label htmlFor="status">
+            Status
+            <select name="status" id="status">
+              <option value="incomplete">Incomplete</option>
+              <option value="complete">Complete</option>
+            </select>
+          </label>
+          <div className={styles.buttonContainer}>
+            <Button type="submit" variant="primary">
+              Add Task
+            </Button>
+            <Button type="button" variant="secondary">
+              Add Task
+            </Button>
+          </div>
         </form>
       </div>
     </div>
