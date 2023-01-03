1d3878850a18730d3a4340319fcf2c12a1f5124f
Deborah <debbyuzuegbu@gmail.com>
2023-01-03 18:30:41 +0100

    created AppContent component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React from 'react';
+import AppContent from './Component/AppContent';
 import AppHeader from './Component/AppHeader';
 import PageTitle from './Component/PageTitle';
-import style from './styles/modules/app.module.scss';
+import styles from './styles/modules/app.module.scss';
 
 function App() {
   return (
     <div className="container">
       <PageTitle>TODO LIST</PageTitle>
-      <div className={style.app__wrapper}>
+      <div className={styles.app__wrapper}>
         <AppHeader />
+        <AppContent />
       </div>
     </div>
   );
--- src/Component/AppHeader.jsx
+++ src/Component/AppHeader.jsx
@@ -5,8 +5,8 @@ import styles from '../styles/modules/app.module.scss';
 function AppHeader() {
   return (
     <div className={styles.appHeader}>
-      <Button variant="primary">Click Me</Button>
-      <SelectButton>
+      <Button variant="primary">Add Task</Button>
+      <SelectButton id="status">
         <option value="all">ALL</option>
         <option value="incomplete">Incomplete</option>
         <option value="complete">Complete</option>
