292378251e2b33651578664d9c7f4614e60ce002
Deborah <debbyuzuegbu@gmail.com>
2023-01-03 18:44:31 +0100

    creating a form modal

--- src/Component/AppHeader.jsx
+++ src/Component/AppHeader.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Button, { SelectButton } from './Button';
 import styles from '../styles/modules/app.module.scss';
+import TodoModal from './TodoModal';
 
 function AppHeader() {
   return (
@@ -11,6 +12,7 @@ function AppHeader() {
         <option value="incomplete">Incomplete</option>
         <option value="complete">Complete</option>
       </SelectButton>
+      <TodoModal />
     </div>
   );
 }
