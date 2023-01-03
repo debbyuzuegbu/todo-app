0ed1e199eb18af5a0d14aaac3cfef12305cca925
Deborah <debbyuzuegbu@gmail.com>
2023-01-03 18:18:05 +0100

    added selectButton

--- src/Component/AppHeader.jsx
+++ src/Component/AppHeader.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
-import Button from './Button';
+import Button, { SelectButton } from './Button';
 
 function AppHeader() {
   return (
     <div>
       <h1>Hello from this part</h1>
       <Button variant="primary">Click Me</Button>
+      <SelectButton>
+        <option value="all">ALL</option>
+        <option value="incomplete">Incomplete</option>
+        <option value="complete">Complete</option>
+      </SelectButton>
     </div>
   );
 }
