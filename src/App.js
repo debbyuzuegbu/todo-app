92bf242e93d0d04f200fef6db74c5fe3a75e1536
Deborah <debbyuzuegbu@gmail.com>
2023-01-04 14:12:27 +0100

    added Toast for push notifications

--- src/App.js
+++ src/App.js
@@ -1,18 +1,30 @@
 import React from 'react';
+import { Toaster } from 'react-hot-toast';
 import AppContent from './Component/AppContent';
 import AppHeader from './Component/AppHeader';
 import PageTitle from './Component/PageTitle';
+
 import styles from './styles/modules/app.module.scss';
 
 function App() {
   return (
-    <div className="container">
-      <PageTitle>TODO LIST</PageTitle>
-      <div className={styles.app__wrapper}>
-        <AppHeader />
-        <AppContent />
+    <>
+      <div className="container">
+        <PageTitle>TODO List</PageTitle>
+        <div className={styles.app__wrapper}>
+          <AppHeader />
+          <AppContent />
+        </div>
       </div>
-    </div>
+      <Toaster
+        position="bottom-right"
+        toastOptions={{
+          style: {
+            fontSize: '1.4rem',
+          },
+        }}
+      />
+    </>
   );
 }
 
