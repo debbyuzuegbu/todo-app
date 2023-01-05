9a2db385c04acd3d2914d2a5e779ce1cc018dd56
Deborah <debbyuzuegbu@gmail.com>
2023-01-05 10:34:56 +0100

    check button

--- src/Component/AppHeader.jsx
+++ src/Component/AppHeader.jsx
@@ -1,19 +1,36 @@
 import React, { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import Button, { SelectButton } from './Button';
 import styles from '../styles/modules/app.module.scss';
 import TodoModal from './TodoModal';
+import { updateFilterStatus } from '../Slices/TodoSlice';
 
 function AppHeader() {
-  const [modalOpen, setModalOpen] = useState(true);
+  const [modalOpen, setModalOpen] = useState(false);
+  const initialFilterStatus = useSelector((state) => state.todo.filterStatus);
+  const [filterStatus, setFilterStatus] = useState(initialFilterStatus);
+  const dispatch = useDispatch();
+
+  const updateFilter = (e) => {
+    setFilterStatus(e.target.value);
+    dispatch(updateFilterStatus(e.target.value));
+  };
+
   return (
     <div className={styles.appHeader}>
-      <Button variant="primary">Add Task</Button>
-      <SelectButton id="status">
-        <option value="all">ALL</option>
+      <Button variant="primary" onClick={() => setModalOpen(true)}>
+        Add Task
+      </Button>
+      <SelectButton
+        id="status"
+        onChange={(e) => updateFilter(e)}
+        value={filterStatus}
+      >
+        <option value="all">All</option>
         <option value="incomplete">Incomplete</option>
-        <option value="complete">Complete</option>
+        <option value="complete">Completed</option>
       </SelectButton>
-      <TodoModal modalOpen={modalOpen} setModalOpen={setModalOpen} />
+      <TodoModal type="add" modalOpen={modalOpen} setModalOpen={setModalOpen} />
     </div>
   );
 }
